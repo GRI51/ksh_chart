f81b5bce9adf78f791829a3e73d7ab3cf523f2f5
GRI <gri.252525@gmail.com>
2023-09-10 23:49:33 +0900

    zip化する際に改行コードを強制的にCRLFに変換、公開日を2023/9/11に変更

--- src/tozip.py
+++ src/tozip.py
@@ -2,6 +2,29 @@
 """
 import shutil
 import os
+import glob
+import warnings
+
+
+def make_zip(zip_name, target_folder):
+    # 改行コードをCRLFに変換
+    glob_path = os.path.join(target_folder, '**', '*.ksh')
+    ksh_list = glob.glob(glob_path, recursive=True)
+    for ksh_path in ksh_list:
+        with open(ksh_path, 'r', encoding='utf-8-sig') as oldfile:
+            txt = oldfile.read()
+        with open(ksh_path, 'w', encoding='utf-8-sig', newline='\r\n') as newfile:
+            newfile.write(txt)
+    # zip化
+    folder_name = os.path.basename(target_folder)
+    dir_name = os.path.dirname(target_folder)
+    try:
+        output_path = shutil.make_archive(
+            zip_name, format='zip', root_dir=dir_name, base_dir=folder_name)
+        print(f'zipファイルの生成に成功しました。出力先：{output_path}')
+    except Exception as err:
+        warnings.warn('zipファイルの生成に失敗しました。')
+        print(err)
 
 
 def export_zip(output_dir=None):
@@ -10,30 +33,20 @@ def export_zip(output_dir=None):
         output_dir = os.path.join(os.path.dirname(
             __file__), '..', 'docs', 'assets')
     # 個別の個人配布譜面をzip化
-    folders = os.listdir(os.path.join(os.path.dirname(__file__), 'songs'))
-    for folder_name in folders:
-        zip_name = os.path.join(output_dir, folder_name)
+    song_folders = os.listdir(os.path.join(os.path.dirname(__file__), 'songs'))
+    for folder_name in song_folders:
         root_dir = os.path.join(os.path.dirname(
             __file__), 'songs', folder_name)
-        try:
-            output_path = shutil.make_archive(
-                zip_name, format='zip', root_dir=root_dir)
-            print(f'zipファイルの生成に成功しました。出力先：{output_path}')
-        except Exception as err:
-            print(err)
-
+        zip_name = os.path.join(output_dir, folder_name)
+        make_zip(zip_name, root_dir)
     # パッケージをzip化
-    folders = os.listdir(os.path.join(os.path.dirname(__file__), 'packages'))
-    for folder_name in folders:
+    package_folders = os.listdir(os.path.join(
+        os.path.dirname(__file__), 'packages'))
+    for folder_name in package_folders:
         zip_name = os.path.join(output_dir, folder_name)
         root_dir = os.path.join(os.path.dirname(
             __file__), 'packages', folder_name)
-        try:
-            output_path = shutil.make_archive(
-                zip_name, format='zip', root_dir=root_dir)
-            print(f'zipファイルの生成に成功しました。出力先：{output_path}')
-        except Exception as err:
-            print(err)
+        make_zip(zip_name, root_dir)
 
 
 if __name__ == '__main__':
