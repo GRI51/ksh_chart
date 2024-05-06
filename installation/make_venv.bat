beb99361c9740728ec86ae598f1ebe40c850feec
GRI <gri.252525@gmail.com>
2024-05-06 17:02:12 +0900

    仮想環境をpipenvで生成するよう変更

--- installation/make_venv.bat
+++ installation/make_venv.bat
@@ -3,49 +3,39 @@ setlocal
 
 set success_make_env=
 set python_ver=3.11
+@REM 仮想環境をプロジェクト直下に作る
+set PIPENV_VENV_IN_PROJECT=1
 
 pushd "%~dp0"
 cd ..
 
 echo Python%python_ver%仮想環境を構築します。
 
-py -%python_ver% -m venv .venv
-if %errorlevel% neq 0 (
-    echo Python%python_ver%が見つかりませんでした。
-    goto end
-)
-
-call .venv\Scripts\activate.bat
-if %errorlevel% neq 0 (
-    echo 仮想環境の実行に失敗しました。
-    goto end
-)
-
-py -m pip install -U pip
+py -%python_ver% -m pip install -U pip
 if %errorlevel% neq 0 (
     echo pipのアップデートに失敗しました。
     goto end
 )
 
-py -m pip install -U setuptools
+py -%python_ver% -m pip install -U setuptools
 if %errorlevel% neq 0 (
     echo setuptoolsのアップデートに失敗しました。
     goto end
 )
 
-py -m pip install pipenv
+py -%python_ver% -m pip install pipenv
 if %errorlevel% neq 0 (
     echo pipenvのアップデートに失敗しました。
     goto end
 )
 
-pipenv install -d -r installation/development.txt
+py -%python_ver% -m pipenv install -d -r installation/development.txt
 if %errorlevel% neq 0 (
     echo 開発用Pythonライブラリのインストールに失敗しました。
     goto end
 )
 
-pipenv install -r installation/requirements.txt
+py -%python_ver% -m pipenv install -r installation/requirements.txt
 if %errorlevel% neq 0 (
     echo Pythonライブラリのインストールに失敗しました。
     goto end
@@ -57,7 +47,7 @@ set success_make_env=True
 popd
 if defined success_make_env (
     echo 環境構築に成功しました！
-    python -V
+    py -%python_ver% -m pipenv run python -V
     pause
     exit /b 0
 ) else (
